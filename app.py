99acdc94d73509f2342f8be2020002f887545dd1
edisonc57 <edisoncontreraspanibra@gmail.com>
2026-08-29 21:29:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st  # Importa la librería Streamlit con el alias st.
-inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página inicial con título, ícono y estado predeterminado.
-api = st.Page("api.py", title="Grado API", icon="🛢️")  # Define la página de cálculo con un título e ícono personalizados.
-pagina = st.navigation([inicio, api])  # Organiza las páginas dentro del menú automático de Streamlit.
-pagina.run()  # Ejecuta únicamente la página seleccionada por el usuario.
+inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página donde se ingresará el dato.
+api = st.Page("api.py", title="Resultado API", icon="🛢️")  # Define la página donde se utilizará el dato guardado.
+pagina = st.navigation([inicio, api])  # Registra las páginas de la aplicación.
+pagina.run()  # Ejecuta la página seleccionada por el usuario.
